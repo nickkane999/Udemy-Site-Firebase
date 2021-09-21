6386751ce2855e88d9c81042123bffe0e75deebc
nickkane999 <nickkane999@gmail.com>
2021-09-21 01:46:39 -0400

    close to finishing

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,10 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Board from './components/Board';
+import History from './components/History';
+import StatusMessage from './components/statusMessage';
+import { calculateWinner } from './helpers';
 
-const App = () => (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+import './styles/root.scss';
+
+const NEW_GAME = [{ board: Array(9).fill(null), isXNext: true }];
+
+const App = () => {
+  const [history, setHistory] = useState(NEW_GAME);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const current = history[currentMove];
+
+  const { winner, winningSquares } = calculateWinner(current.board);
+
+  const handleSquareClick = position => {
+    if (current.board[position] || winner) {
+      return;
+    }
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((square, pos) => {
+        if (pos === position) {
+          return last.isXNext ? 'X' : 'O';
+        }
+        return square;
+      });
+
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
+    });
+
+    setCurrentMove(prev => prev + 1);
+  };
+
+  const moveTo = move => {
+    setCurrentMove(move);
+  };
+
+  const onNewGame = () => {
+    setHistory([NEW_GAME]);
+    setCurrentMove(0);
+  };
+
+  return (
+    <div className="app">
+      <h1>Tic Tac Toe</h1>
+      <StatusMessage winner={winner} current={current} />
+      <Board
+        board={current.board}
+        handleSquareClick={handleSquareClick}
+        winningSquares={winningSquares}
+      />
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
+    </div>
+  );
+};
 
 export default App;
